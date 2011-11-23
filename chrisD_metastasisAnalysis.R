e45002abce053192bf61a3ae11cebca84c9a61ef
Christiaan Klijn <c.klijn@gmail.com>
2011-11-23 13:42:16 +0100

    Rewritten the code to produce the diff figures, now uses internal functions and mclapply to speed it up

--- chrisD_metastasisAnalysis.R
+++ chrisD_metastasisAnalysis.R
@@ -7,82 +7,139 @@
 
 # Run on medoid
 
-library(GenomicRanges)
-library(gplots)
-data(mmMirrorLocs)
 setwd("/home/klijn/data/smallproj/chrisD/")
+# Data
 load('rawData_chrisD.Rda')
 load('chrisD_segmentedDelta.Rda')
+# Libraries and functions
+library(multicore) # for parrallel preprocessing, works only on linux
 source('~/gitCodeChris/generalFunctionsR/chris_cghdata_analysis.R')
 source('~/gitCodeChris/chrisD/chrisD_functions.R')
 
-# Only count segments with more than 5 probes and set everything between the thresholds to 0
+# -------------------------------------------
+# Local functions
 
-threshold <- .2
+preprocessForDiffFigures <- function(threshold, segData, 
+  diffKCrecMet, minMark=5) {
+    
+  # This function does all the plotting given a threshold on the delta
+  # segments and a minimum number of markers
 
-segData <- segRecMet$output
-segData$seg.mean[segData$num.mark < 5] <- 0
-segData$seg.mean[abs(segData$seg.mean) < threshold] <- 0
-segData$seg.mean[segData$seg.mean < -threshold] <- -1
-segData$seg.mean[segData$seg.mean > threshold] <- 1
+  segData$seg.mean[segData$num.mark < 5] <- 0
+  segData$seg.mean[abs(segData$seg.mean) < threshold] <- 0
+  segData$seg.mean[segData$seg.mean < -threshold] <- -1
+  segData$seg.mean[segData$seg.mean > threshold] <- 1
 
-# Fix a weird renaming thing in DNAcopy
-# replace the three consequtive dots with a dash
-segData$ID <- gsub('[.]{3}', ' - ', segData$ID)
+  # Fix a weird renaming thing in DNAcopy
+  # replace the three consequtive dots with a dash
+  segData$ID <- gsub('[.]{3}', ' - ', segData$ID)
 
-# Set probevalues to segment values
+  # Set probevalues to segment values
 
-diffKCrecMet <- diffKCrecMet[
-  order(diffKCrecMet$chrom, diffKCrecMet$maploc),]
-diffKCrecMet.Seg <- diffKCrecMet
+  diffKCrecMet <- diffKCrecMet[
+    order(diffKCrecMet$chrom, diffKCrecMet$maploc),]
+  diffKCrecMet.Seg <- diffKCrecMet
 
-for (s in 1:nrow(segData)) {
-  colNum <- which(colnames(diffKCrecMet.Seg) == segData$ID[s])
-  probeInd <- which(diffKCrecMet.Seg$chrom == segData$chrom[s] &
-    diffKCrecMet.Seg$maploc >= segData$loc.start[s] &
-    diffKCrecMet.Seg$maploc <= segData$loc.end[s])
-  diffKCrecMet.Seg[probeInd, colNum] <- segData$seg.mean[s]
+  for (s in 1:nrow(segData)) {
+    colNum <- which(colnames(diffKCrecMet.Seg) == segData$ID[s])
+    probeInd <- which(diffKCrecMet.Seg$chrom == segData$chrom[s] &
+      diffKCrecMet.Seg$maploc >= segData$loc.start[s] &
+      diffKCrecMet.Seg$maploc <= segData$loc.end[s])
+    diffKCrecMet.Seg[probeInd, colNum] <- segData$seg.mean[s]
+  }
+
+  returnList <- list(
+    diffKCSeg=diffKCrecMet.Seg,
+    threshold=threshold,
+    minMark=minMark
+    )
+
+  return(returnList)
+
 }
 
-recep <- c('R1', 'R2', 'R3')
+plotAllDiffFigures <- function(diffKCSegList) {
+  
+  # Make listitems in the the diffKCSegList local variables
+  attach(diffKCSegList)
+  
+  recep <- c('R1', 'R2', 'R3')
 
-for (r in recep) {
-  png(file=paste('Figures/', r, 'priMetaDiff_02_black.png', sep=''), 
-    width=1000, height=800)
-  metMat <- as.matrix(diffKCrecMet.Seg[,
-    grepl(r, colnames(diffKCrecMet.Seg))])
-  plotMetMat(metMat, diffKCrecMet.Seg, 
-    plotTitle=paste(r, 'primary-metastasis CNA diff'))
+  # --------
+  # Recepients and their metas
+
+  for (r in recep) {
+    metMat <- as.matrix(diffKCSeg[,
+      grepl(r, colnames(diffKCSeg))])
+    png(file=
+      paste('Figures/', r, 'priMetaDiff_', threshold, '.png', sep=''), 
+      width=1000, height=800)
+    plotMetMat(metMat, diffKCSeg, 
+      plotTitle=paste(r, 'primary-metastasis CNA diff'))
+    dev.off()
+    postscript(file=
+      paste('Figures/', r, 'priMetaDiff_', threshold, '.eps', sep=''), 
+      width=10, height=8, paper='special', horizontal=F)
+    plotMetMat(metMat, diffKCSeg, 
+      plotTitle=paste(r, 'primary-metastasis CNA diff'))
+    dev.off()
+  }
+
+  # ---------
+  # All lung metas
+
+  metMat <- as.matrix(diffKCSeg[,
+     grepl('Lung', colnames(diffKCSeg))])
+  
+  png(file=paste('Figures/allLungMetsDiff', threshold, '.png', sep=''),
+    width=1000, height=800)  
+  plotMetMat(metMat, diffKCSeg, 
+    plotTitle=
+      paste('All lung primary-metastasis CNA diff -', threshold))
   dev.off()
+  
+  postscript(file=
+    paste('Figures/allLungMetsDiff', threshold, '.eps', sep=''),
+    width=10, height=8, paper='special', horizontal=F)
+  plotMetMat(metMat, diffKCSeg, 
+    plotTitle=
+      paste('All lung primary-metastasis CNA diff -', threshold))
+  dev.off()
+
+  # ---------
+  # All LN metas
+
+  metMat <- as.matrix(diffKCSeg[,
+     grepl('LN', colnames(diffKCSeg))])
+
+  png(file=
+    paste('Figures/allLNMetsDiff', threshold, '.png', sep=''), 
+    width=1000, height=800)
+  plotMetMat(metMat, diffKCSeg, 
+      plotTitle=
+        paste('All lymphnode primary-metastasis CNA diff -', threshold))
+  dev.off()
+
+  postscript(file=
+    paste('Figures/allLNMetsDiff', threshold, '.eps', sep=''), 
+    width=10, height=8, paper='special', horizontal=F)
+  plotMetMat(metMat, diffKCSeg, 
+      plotTitle=
+        paste('All lymphnode primary-metastasis CNA diff -', threshold))
+  dev.off()
+
 }
 
-png(file="Figures/allLungMetsDiff.png", width=1000, height=800)
-metMat <- as.matrix(diffKCrecMet.Seg[,
-   grepl('Lung', colnames(diffKCrecMet.Seg))])
-plotMetMat(metMat, diffKCrecMet.Seg, 
-    plotTitle='All lung primary-metastasis CNA diff')
-dev.off()
+# -------------------------------------------
+# Actual code
 
-png(file="Figures/allLNMetsDiff.png", width=1000, height=800)
-metMat <- as.matrix(diffKCrecMet.Seg[,
-   grepl('LN', colnames(diffKCrecMet.Seg))])
-plotMetMat(metMat, diffKCrecMet.Seg, 
-    plotTitle='All lymph node primary-metastasis CNA diff')
-dev.off()
+# Get just the segments infomation
+segData <- segRecMet$output
 
-# -----------
+# Run preprocessing for two thresholds
+thresholdList <- list(thres01=.1, thres02=.2)
+KCSegList <- mclapply(thresholdList, preprocessForDiffFigures, segData,
+  diffKCrecMet)
 
-
-segDataFilter <- segData[segData$num.mark > 5 & 
-  abs(segData$seg.mean) > .1,]
-
-# Get a ranges object
-
-filterSegs <- with(segDataFilter, GRanges(
-  seqnames = Rle(paste('chr', chrom)),
-  ranges=IRanges(start=loc.start,end=loc.end), 
-  seg.mean=seg.mean, 
-  num.mark=num.mark,
-  ID=ID))
-
-tumSegs <- filterSegs[grepl('R1', elementMetadata(filterSegs)$ID),]
+# Plot the delta figures using the preprocessed data
+mclapply(KCSegList, plotAllDiffFigures)
